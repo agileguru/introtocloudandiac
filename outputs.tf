4d74da4c6f458fc665e4cf79c22686892c7f7cfd
Guru Raghupathy <guru@doit-intl.com>
2023-03-17 18:15:31 +0000

    Added and cleaning output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,7 @@
-# output "dev_db_hostname" {
-#   value = module.dev_db.db_hostname
-# }
-
-# output "dev_db_type_version" {
-#   value = module.dev_db.database_type
-# }
-
-# output "dev_db_instance_id" {
-#   value = module.dev_db.db_instance_id
-# }
-
-output "lb_ip_address" {
+output "dev_lb_ip_address" {
   value = module.common_load_balancer.lb_ip_address
+}
+
+output "akshay_lb_ip_address" {
+  value = module.akshay_load_balancer.lb_ip_address
 }
